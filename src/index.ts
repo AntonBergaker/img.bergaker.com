bb855b388638db6d9ebb64aa7e2253045040629c
Anton <noten.b@hotmail.com>
2019-06-02 14:17:55 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,118 @@
+import express = require('express');
+import multer = require('multer');
+import crypto = require('crypto');
+import fs = require('fs');
+import path = require('path');
+
+const app = express();
+
+const imgDirectory = 'images/';
+const vidDirectory = 'videos/';
+
+const port = 80;
+
+function chooseFilename(directory : string, extension : string) : string {
+	let path;
+	do {
+		path = crypto.randomBytes(3).toString('base64') + extension;
+	} while(fs.existsSync(directory + path))
+	return path;
+}
+
+const imgStorage = multer.diskStorage( {
+	destination: function(req, file, cb) {
+		cb(null, imgDirectory);
+	},
+	filename: function (req, file, cb) {
+		cb(null, chooseFilename(imgDirectory, path.extname(file.originalname)))
+	}
+})
+const vidStorage = multer.diskStorage( {
+	destination: function(req, file, cb) {
+		cb(null, vidDirectory);
+	},
+	filename: function (req, file, cb) {
+		cb(null, chooseFilename(vidDirectory, path.extname(file.originalname)))
+	}
+})
+
+
+const imgUpload = multer({storage: imgStorage})
+const vidUpload = multer({storage: vidStorage})
+
+app.set('port', (process.env.PORT || port));
+app.set('view engine', 'pug');
+
+function authorize(req: express.Request, res: express.Response, next: express.NextFunction) {
+	if (req.headers.Authorization == "notasecret") {
+		res.sendStatus(403);
+	} else {
+		next();
+	}
+}
+
+app.post('/upload_image',
+	authorize,
+	imgUpload.single('file_image'),
+	(req, res) => {
+		res.type('application/json').send( JSON.stringify({url: "https://img.bergaker.com/" + req.file.filename}));
+	}
+);
+app.post('/upload_video',
+	authorize,
+	vidUpload.single('file_video'),
+	(req, res) => {
+		res.type('application/json').send( JSON.stringify({url: "https://vid.bergaker.com/" + path.basename(req.file.filename, path.extname(req.file.filename))}));
+	}
+);
+
+app.get('*', (req, res) => {
+	if (req.path.length < 4) {
+		res.sendStatus(404);
+		return;
+	}
+
+	if ((req.subdomains.length > 0 && req.subdomains[0] == "vid") || req.query.video == "1") {
+		const fileId = req.path.replace('/', '');
+		const fullPath = vidDirectory + fileId;
+		const hasExt = path.extname(fullPath) != "";
+		const filePath = hasExt ? fullPath : fullPath + ".mp4";
+
+		if (fs.existsSync(filePath) == false) {
+			res.sendStatus(404);
+			return;
+		}
+
+		// With extension, serve the file itself
+		if (hasExt) {
+			res.contentType('video/' + path.extname(filePath));
+			res.end(fs.readFileSync(filePath));
+		}
+		// If no extension, serve a website to view it
+		else {
+			const query = (req.query.video == "1") ? "?video=1" : ""
+			res.render("video", {
+				fileId: fileId,
+				filePath: fileId + ".mp4" + query,
+				fileExt: "mp4"
+			})
+
+		}
+
+
+	} else {
+		// look for the file
+		const fullPath = imgDirectory + req.path.replace('/', '');
+		if (fs.existsSync(fullPath) == false) {
+			res.sendStatus(404);
+			return;
+		}
+
+		res.contentType('image/' + path.extname(fullPath));
+		res.end(fs.readFileSync(fullPath));
+	}
+
+
+});
+
+app.listen(80);
